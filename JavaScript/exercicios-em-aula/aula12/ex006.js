43f53757f127c6a7934a62e54f0153111820415d
Gilberto Carlos de Oliveira Silva <102378685+SilvaGilberto@users.noreply.github.com>
2022-04-05 23:22:32 -0300

    Finalizando o dia

---
+++ JavaScript/exercicios-em-aula/aula12/ex006.js
@@ -0,0 +1,9 @@
+let horario = 19
+console.log(`São ${horario}h`)
+if (horario < 12) {
+    console.log('Bom dia!')
+} else if (horario <= 18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa Noite!')
+}
